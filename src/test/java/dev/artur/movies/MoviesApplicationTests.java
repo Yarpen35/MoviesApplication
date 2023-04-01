aba2c96890ae31f218032c3c7de7ce077dee097a
Yarpen <71726390+Yarpen35@users.noreply.github.com>
2023-04-01 15:47:25 +0200

    RestController

---
+++ src/test/java/dev/artur/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.artur.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
